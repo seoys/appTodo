d19e9d5985face23287e1822b8809d83834e351b
young sik seo <angelskying@gmail.com>
2022-09-27 18:15:38 +0900

    init

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { Images, products, materialTheme } from './constants'
+import { Block, GalioProvider } from 'galio-framework';
+import { NavigationContainer } from '@react-navigation/native';
+import Screens from './navigation/Screens'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+// cache app images
+const assetImages = [
+  Images.Pro,
+  Images.Profile,
+  Images.Avatar,
+  Images.Onboarding,
+];
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === 'string') {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+
+  state = {
+    isLodingComplete : false,
+  }
+
+  render() {
+
+    return (
+        <NavigationContainer>
+          <GalioProvider theme={materialTheme}>
+            <Block flex>
+              {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+              <Screens/>
+            </Block>
+           </GalioProvider>
+        </NavigationContainer>
+      )
+  }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([
+      ...cacheImages(assetImages),
+    ]);
+  };
+
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    console.log('aaaaa')
+    this.setState({ isLoadingComplete: true });
+  };
+}
+
---
+++ navigation/Screens.js
@@ -0,0 +1,134 @@
+import { Animated, Dimensions, Easing } from "react-native";
+import { Block, Text, theme } from "galio-framework";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import OnboardingScreen from "../screens/Onboarding";
+import { Header, Icon } from "../components";
+import { Images, materialTheme } from "../constants/";
+
+import HomeScreen from "../screens/Home";
+import ProScreen from "../screens/Pro";
+
+import CustomDrawerContent from "./Menu";
+
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const { width } = Dimensions.get("screen");
+
+const profile = {
+  avatar: Images.Profile,
+  name: "Rachel Brown",
+  type: "Seller",
+  plan: "Pro",
+  rating: 4.8,
+};
+
+function HomeStack(props) {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        mode: "card",
+        headerShown: "screen",
+      }}
+    >
+      <Stack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          header: ({ navigation, scene }) => (
+            <Header
+              search
+              tabs
+              title="Home"
+              navigation={navigation}
+              scene={scene}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen
+        name="Pro"
+        component={ProScreen}
+        options={{
+          header: ({ navigation, scene }) => (
+            <Header
+              back
+              white
+              transparent
+              title=""
+              navigation={navigation}
+              scene={scene}
+            />
+          ),
+          headerTransparent: true,
+        }}
+      />
+    </Stack.Navigator>
+  );
+}
+
+function AppStack(props) {
+    return (
+        <Drawer.Navigator
+      style={{ flex: 1 }}
+      drawerContent={(props) => (
+        <CustomDrawerContent {...props} profile={profile} />
+      )}
+      drawerStyle={{
+        backgroundColor: "white",
+        width: width * 0.8,
+      }}
+      drawerContentOptions={{
+        activeTintColor: "white",
+        inactiveTintColor: "#000",
+        activeBackgroundColor: materialTheme.COLORS.ACTIVE,
+        inactiveBackgroundColor: "transparent",
+        itemStyle: {
+          width: width * 0.74,
+          paddingHorizontal: 12,
+          // paddingVertical: 4,
+          justifyContent: "center",
+          alignContent: "center",
+          // alignItems: 'center',
+          overflow: "hidden",
+        },
+        labelStyle: {
+          fontSize: 18,
+          fontWeight: "normal",
+        },
+      }}
+      initialRouteName="Home"
+        >
+            <Drawer.Screen
+        name="Home"
+        component={HomeStack}
+        options={{
+          drawerIcon: ({ focused }) => (
+            <Icon
+              size={16}
+              name="shop"
+              family="GalioExtra"
+              color={focused ? "white" : materialTheme.COLORS.MUTED}
+            />
+          ),
+        }}
+      />
+    </Drawer.Navigator>
+    )
+}
+
+export default function OnboardingStack(props) {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Onboarding"
+                component={OnboardingScreen}
+                option={{
+                    headerTransparent: true
+                }}
+            />
+            <Stack.Screen name="App" component={AppStack} />
+        </Stack.Navigator>
+    )
+}
